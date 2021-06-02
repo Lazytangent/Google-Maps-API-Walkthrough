c63bfba66e3ebaaeedf2a57931ecd22644d8e81d
Lazytangent <pm123633@gmail.com>
2021-06-02 14:42:54 -0500

    Test signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post('', asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+  const user = await User.signup({ email, username, password });
+
+  setTokenCookie(res, user);
+
+  res.json({
+    user,
+  });
+}));
 
 module.exports = router;
