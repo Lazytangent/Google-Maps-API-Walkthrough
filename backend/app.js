01ff934d34f32039a6dcba0f21460f34534785d7
Lazytangent <pm123633@gmail.com>
2021-06-02 12:30:14 -0500

    Phase 2

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const routes = require('./routes');
@@ -37,5 +38,34 @@ app.use(
 app.use(routes);
 
 // Error handlers go AFTER routes
+// Route Not Found Error Handler
+app.use((_req, _res, next) => {
+  const err = new Error('The requested resource couldn\'t be found.');
+  err.title = 'Resource Not Found';
+  err.errors = ['The requested resource couldn\'t be found.'];
+  err.status = 404;
+  next(err);
+});
+
+// Sequelize Validation Error Handler
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error Formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
