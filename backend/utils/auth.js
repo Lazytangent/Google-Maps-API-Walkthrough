1981939435da6e12c08a9ae5aca4dbe1d3703ecb
Lazytangent <pm123633@gmail.com>
2021-06-02 13:12:37 -0500

    Create auth middlewares and helper

---
+++ backend/utils/auth.js
@@ -0,0 +1,63 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig: { secret, expiresIn } } = require('../config');
+const { User } = require('../db/models');
+
+const setTokenCookie = (res, user) => {
+  const token = jwt.sign(
+    { data: user.toSafeObject() },
+    secret,
+    { expiresIn: parseInt(expiresIn) },
+  );
+
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  res.cookie('token', token, {
+    maxAge: expiresIn * 1000,
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction & 'Lax',
+  });
+
+  return token;
+};
+
+// Middleware function
+const restoreUser = (req, res, next) => {
+  const { token } = req.cookiets;
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+const requireAuth = [
+  restoreUser,
+  function(req, res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+  },
+];
+
+module.exports = {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+};
