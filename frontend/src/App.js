dd5bc801a5f2194dc0828fdc4422f4fb4ed764a1
Lazytangent <pm123633@gmail.com>
2021-06-03 12:20:51 -0500

    Create and connect Login Form component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import { Switch, Route } from "react-router-dom";
+
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Hello from App</h1>
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ frontend/src/components/LoginFormPage/LoginFormPage.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+import { login } from "../../store/session";
+import styles from "./LoginFormPage.module.css";
+
+const LoginFormPage = () => {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  console.log(errors);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(login({ credential, password }));
+    } catch (err) {
+      (async () => {
+        const { errors } = await err.json();
+        setErrors(errors);
+      })();
+    }
+  };
+
+  if (sessionUser) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <form onSubmit={submitHandler} className={styles.form}>
+      <h3 className={styles.heading}>Login</h3>
+      {errors.length > 0 && (
+        <ul className={styles.errors}>
+          {errors.map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Username or Email"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <button type="submit" className={styles.button}>
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginFormPage;
