91c4ac9db091b58141a5edca32a34f4ba3b11b19
Lazytangent <pm123633@gmail.com>
2021-06-03 10:08:38 -0500

    Wrapping up the Backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+/** Route for testing backend API routes */
+// router.get('/hello/world', (req, res) => {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
