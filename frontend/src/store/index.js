9d12e4f48d16991582529aefbd08eef0d6b2fbfb
Lazytangent <pm123633@gmail.com>
2021-06-04 13:33:39 -0500

    Fix some stuff

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,8 +12,10 @@ const rootReducer = combineReducers({
 let enhancer;
 
 if (process.env.NODE_ENV === 'production') {
+  // Just thunking
   enhancer = applyMiddleware(thunk);
 } else {
+  // Thunk and logs
   const logger = require('redux-logger').default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
